b142f3ccf5bd3a77a33cadeebaac78da6a833777
Floaty_KT <kasemtan88@hotmail.co.th>
2022-07-02 14:38:20 +0700

    update: add function

--- pedeeepe-a/src/app.tsx
+++ pedeeepe-a/src/app.tsx
@@ -1,13 +1,23 @@
+import RegisterDialog from "./components/dialog/RegisterDialog";
+import { h } from "preact";
+import { useEffect } from "preact/hooks";
+
 export function App() {
+  const handleRegister = () => {
+    const modal = document.getElementById("register-dialog");
+    if (modal) {
+      setTimeout(() => {
+        modal.style.display = "block";
+      }, 1000);
+    }
+  };
+  useEffect(() => {
+    handleRegister();
+  }, []);
+
   return (
     <>
-      <button
-        type="button"
-        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-      >
-        Light
-      </button>
-      <div className="h-screen bg-black"></div>
+      <RegisterDialog id="register-dialog"></RegisterDialog>
     </>
   );
 }
