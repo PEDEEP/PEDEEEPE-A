da11d1bf321b3bb6d79ca18fe083524116cbc5e1
Floaty_KT <kasemtan88@hotmail.co.th>
2022-07-02 16:11:24 +0700

    feat: add onclick function

--- pedeeepe-a/src/home.tsx
+++ pedeeepe-a/src/home.tsx
@@ -1,30 +1,35 @@
-import './styles/home.css'
-import logo from'./image/logo.png'
-import tiger from './image/tiger.gif'
+import "./styles/home.css";
+import logo from "./image/logo.png";
+import tiger from "./image/tiger.gif";
+import Swal from "sweetalert2";
 
+const emptyArray = [1, 1, 1, 1, 1, 1];
 export function Home() {
   return (
     <>
-      <img class="logo-img" 
-      src={logo} alt="Logo" />
-      
-      <h1>STUPID HACKATON SCHOOL</h1>
-      <marquee>ยินดีต้อนรับสู่เว็ปไซต์......สถานศึกษาสตูปิดแฮคคาตอน.......สถาบันเพื่อโปรแกรมเมอร์</marquee>
-      <div class="topnav">
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-      <a href="#home">Home</a>
-</div> 
+      <img class="logo-img" src={logo} alt="Logo" />
 
-    <div className="image-container"> 
-      <img src={tiger} alt="tiger"/>
-      <img src={tiger} alt="tiger"/>
-    </div>
+      <h1>STUPID HACKATON SCHOOL</h1>
+      <marquee>
+        ยินดีต้อนรับสู่เว็ปไซต์......สถานศึกษาสตูปิดแฮคคาตอน.......สถาบันเพื่อโปรแกรมเมอร์
+      </marquee>
+      <div class="topnav">
+        {emptyArray.map(() => (
+          <a
+            href="#home"
+            onClick={() => {
+              Swal.fire("มีแค่หน้าเดี่ยวครับ ทำไว้เฉยๆ");
+            }}
+          >
+            Home
+          </a>
+        ))}
+      </div>
+
+      <div className="image-container">
+        <img src={tiger} alt="tiger" />
+        <img src={tiger} alt="tiger" />
+      </div>
     </>
   );
 }
