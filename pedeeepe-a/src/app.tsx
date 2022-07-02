13a932e1b2acace8c2cd8ed6abd5d5197e6c3985
Floaty_KT <kasemtan88@hotmail.co.th>
2022-07-02 23:12:55 +0700

    update: styling

--- pedeeepe-a/src/app.tsx
+++ pedeeepe-a/src/app.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "preact/hooks";
 import Home from "./home";
 import GamblingAdsLeft from "./components/ads/GamblingAdsLeft";
 import GamblingAdsRight from "./components/ads/GamblingAdsRight";
+import Question from "./components/quiz/Quiz";
 
 export function App() {
   const handleRegister = () => {
@@ -15,6 +16,8 @@ export function App() {
     }
   };
   useEffect(() => {
+    const main = document.getElementsByTagName("body")[0];
+    if (main) main.style.overflow = "hidden";
     handleRegister();
   }, []);
 
@@ -24,6 +27,25 @@ export function App() {
       <RegisterDialog id="register-dialog"></RegisterDialog>
       <GamblingAdsLeft />
       <GamblingAdsRight />
+      <div
+        id="quiz"
+        style={{
+          display: "none",
+          position: "absolute",
+          top: 0,
+          zIndex: 9999,
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "white",
+        }}
+      >
+        <Question
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </div>
     </div>
   );
 }
