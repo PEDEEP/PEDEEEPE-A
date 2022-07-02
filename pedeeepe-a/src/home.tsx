115bd5ef43e1161f987cacca0a8ae3e848ca7876
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-02 19:41:53 +0700

    Add content zone

--- pedeeepe-a/src/home.tsx
+++ pedeeepe-a/src/home.tsx
@@ -3,6 +3,13 @@ import logo from "./image/logo.png";
 import tiger from "./image/tiger.gif";
 import phoom from "./image/Poom.gif"
 import Swal from "sweetalert2";
+import content1 from "./image/content-image/img-content1.jpg"; 
+import content2 from "./image/content-image/img-content2.jpg"; 
+import content3 from "./image/content-image/img-content3.jpg"; 
+import content4 from "./image/content-image/img-content4.jpg"; 
+import content5 from "./image/content-image/img-content5.jpg"; 
+import content6 from "./image/content-image/img-content6.jpg"; 
+
 
 const emptyArray = [1, 1, 1, 1, 1, 1];
 export function Home() {
@@ -32,15 +39,27 @@ export function Home() {
         <img src={phoom} alt="phoom " />
       </div>
 
+    <h2>ประชาสัมพันธ์องค์กร</h2>
     <div class="wrapper">
       <div>
-        <img src="" alt="" />
+        <img src={content1} alt="content-img1" />
       </div>
-      <div>Two</div>
-      <div>Three</div>
-      <div>Four</div>
-      <div>Five</div>
-      <div>Six</div>
+      <div>
+        <img src={content2} alt="content-img2" />
+      </div>
+      <div>
+        <img src={content3} alt="content-img3" />
+      </div>
+      <div>
+        <img src={content4} alt="content-img4" />
+      </div>
+      <div>
+        <img src={content5} alt="content-img5" />
+      </div>
+      <div>
+        <img src={content6} alt="content-img6" />
+      </div>
+
     </div>
     </>
   );
