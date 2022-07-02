951522d32094bd33b48ad5b99978c9fc477991f8
Floaty_KT <kasemtan88@hotmail.co.th>
2022-07-02 13:13:07 +0700

    feat: add tailwindcss

--- pedeeepe-a/src/app.tsx
+++ pedeeepe-a/src/app.tsx
@@ -1,17 +1,13 @@
 export function App() {
   return (
     <>
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
+      <button
+        type="button"
+        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      >
+        Light
+      </button>
+      <div className="h-screen bg-black"></div>
     </>
   );
 }
